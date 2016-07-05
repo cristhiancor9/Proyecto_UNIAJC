fb1f862bb1070a4549ff8e6b1896c7c5f25be4fa
cesar <negociemos1984@hotmail.com>
2016-07-05 15:50:33 -0500

    update3

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
-<!doctype html>
-<html lang=”en”>
-<head>
-<meta charset=”utf-8″>
-<meta http-equiv=”X-UA-Compatible” content=”IE=edge,chrome=1″>
-<title>Demo para el blog</title>
-</head>
-<body>
-<h1>
-Demo para el blog https://xmeele.wordpress.com
-</h1>
-<p>
-Ahora veran los cambios en el index.
-</p>
-</body>
-</html>
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
